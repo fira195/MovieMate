7ae0030d940dc8e7483fd6603c5c254aa3cc0ae1
Firaol <firaolts195@gmail.com>
2024-08-07 17:11:18 +0300

    server

--- server/controller/userAuth.js
+++ server/controller/userAuth.js
@@ -1,8 +1,10 @@
+import User from '../model/userModel.js'
+import bcrypt from 'bcryptjs'
 const createUser = async (req, res) => {
   try {
     const { username, password, email } = req.body;
     const hashedPassword = bcrypt.hashSync(password);
-    await userModel.create({ username, password: hashedPassword, email });
+    await User.create({ username, password: hashedPassword, email });
     res.status(201).json({ message: "User Created" });
   } catch (e) {
     res.status(500).json({ message: "Internal Server Error" });
@@ -12,7 +14,7 @@ const createUser = async (req, res) => {
 const login = async (req, res) => {
   try {
     const { username, password } = req.body;
-    const user = await userModel.findOne({ username });
+    const user = await User.findOne({ username });
     if (!user) return res.status(404).json({ message: "User Not Found" });
     const isMatch = bcrypt.compareSync(password, user.password);
     if (!isMatch) return res.status(401).json({ message: "Unauth" });
